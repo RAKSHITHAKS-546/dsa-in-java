c556c22547773179ce1ff096305d80bae852c3fe
Rakshitha KS <rakshithaks546@gmail.com>
2025-11-25 22:20:32 +0530

    Add files via upload

---
+++ DSA IN JAVA/array1.java
@@ -0,0 +1,12 @@
+public class array1{
+    public static void main(String[] args) {
+        int i=0;
+        int[] x={6,9,10,13,14,19,34};
+        for(i=0;i<=6;i++)
+        {
+            System.out.println(x[i]);
+        }
+        
+        
+    }
+}
---
+++ DSA IN JAVA/arraylength.java
@@ -0,0 +1,14 @@
+public class arraylength {
+    public static void main(String[] args) {
+        int n;
+        int[] arr={2,3,4,5,6,789,23,3,34,4,5,5};
+        System.out.println("Length of the array : "+arr.length);
+        n=arr.length;
+        int i=0;
+        for(i=0;i<=n;i++)
+        {
+            System.out.println(arr[i]+" ");
+        }
+    }
+    
+}
